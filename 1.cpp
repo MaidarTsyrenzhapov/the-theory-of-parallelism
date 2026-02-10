41c1d8f4a5cd7e207373b2c0c33aa4ea9a97972a
MaidarTsyrenzhapov <m.tsyrenzhapov@g.nsu.ru>
2026-02-10 13:12:30 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <vector>
 
-const double PI = 3.141592653589793;
+const double PId = 3.141592653589793;
+const float PIf = 3.141592653589793;
 
-double my_sin(double x) {
+double my_sin_double(double x) {
 
-    while (x > PI) x -= 2 * PI;
-    while (x < -PI) x += 2 * PI;
+    while (x > PId) x -= 2 * PId;
+    while (x < -PId) x += 2 * PId;
+    
+    double res = x;
+    double x2 = x * x;
+    double pow = x;
+    
+    pow *= x2; res -= pow / 6.0;
+    pow *= x2; res += pow / 120.0;
+    pow *= x2; res -= pow / 5040.0;
+    pow *= x2; res += pow / 362880.0; 
+    
+    return res;
+}
+double my_sin_float(float x) {
+
+    while (x > PIf) x -= 2 * PIf;
+    while (x < -PIf) x += 2 * PIf;
     
     double res = x;
     double x2 = x * x;
@@ -23,21 +40,32 @@ double my_sin(double x) {
 int main() {
     const size_t N = 10000000;
     
-    std::vector<double> sin_array(N);
-    
+    std::vector<double> sin_array1(N);
+    std::vector<float> sin_array2(N);
+
     // Заполняем массив
     for (size_t i = 0; i < N; ++i) {
-        double x = 2.0 * PI * i / N;
-        sin_array[i] = my_sin(x);
+        double x1 = 2.0 * PId * i / N;
+        sin_array1[i] = my_sin_double(x1);
+    }
+
+    for (size_t i = 0; i < N; ++i) {
+        float x2 = 2.0 * PIf * i / N;
+        sin_array2[i] = my_sin_float(x2);
     }
     
     // Сумма
-    double sum = 0.0;
-    for (double val : sin_array) {
-        sum += val;
+    double sum1 = 0.0;
+    for (double val : sin_array1) {
+        sum1 += val;
+    }
+    float sum2 = 0.0;
+    for (float val : sin_array2) {
+        sum2 += val;
     }
     
-    std::cout << "Сумма my_sin(x): " << sum << std::endl;
+    std::cout << "Сумма my_sin_double(x): " << sum1 << std::endl;
+    std::cout << "Сумма my_sin_float(x): " << sum2 << std::endl;
     
     return 0;
 }
